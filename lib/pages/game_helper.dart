8bdf6323f88403963a9648cc4c2e031438d1b974
Сократов Андрей (Sokratov_AV) <Sokratov_AV@directum.ru>
2024-03-29 14:26:28 +0400

    Random game choser fix

--- lib/pages/game_helper.dart
+++ lib/pages/game_helper.dart
@@ -12,9 +12,10 @@ class GameHelper extends StatefulWidget {
 class _GameHelperState extends State<GameHelper> {
   double minPlayersValue = 1;
   double maxPlayersValue = 4;
-  String chosenGame = "No game was chosen";
   double chosenPlayersCount = 4;
-  RangeValues maxPlayersRangeValues = const RangeValues(0, 0);
+  String chosenGame = "No game was chosen";
+  RangeValues minRangeValues = const RangeValues(1, 4);
+  RangeValues maxRangeValues = const RangeValues(0, 0);
   bool? onlyOwnedGames = true;
   @override
   Widget build(BuildContext context) {
@@ -58,20 +59,19 @@ class _GameHelperState extends State<GameHelper> {
                     child: Column(
                       children: [
                         RangeSlider(
-                          values: maxPlayersRangeValues,
-                          max: 12,
-                          divisions: 12,
+                          values: maxRangeValues,
+                          max: 10,
+                          divisions: 10,
                           labels: RangeLabels(
-                            maxPlayersRangeValues.start.round().toString(),
-                            maxPlayersRangeValues.end.round().toString(),
-                          ),
+                              maxRangeValues.start.round().toString(),
+                              maxRangeValues.end.round().toString()),
                           onChanged: (RangeValues values) {
                             setState(() {
-                              maxPlayersRangeValues = values;
+                              maxRangeValues = values;
                             });
                           },
                         ),
-                        const Text("Maximum players limit"),
+                        const Text("Max players count"),
                       ],
                     ))),
             Checkbox(
@@ -85,24 +85,18 @@ class _GameHelperState extends State<GameHelper> {
             ElevatedButton(
               child: Text("Chose game"),
               onPressed: () async {
-                // if (maxPlayersCount < chosenPlayersCount &&
-                //     maxPlayersCount != 0)
-                //   setState(() {
-                //     maxPlayersCount = chosenPlayersCount;
-                //   });
                 var allGames = await GameThingSQL.getAllGames();
                 List<GameThing> filteredGames = [];
                 if (allGames == null) return;
                 for (var game in allGames) {
                   print(
                       "Game = ${game.name}, min = ${game.minPlayers}, max = ${game.maxPlayers}");
-
                   if (game.minPlayers <= chosenPlayersCount &&
                           game.maxPlayers >= chosenPlayersCount &&
-                          maxPlayersRangeValues.end != 0
-                      ? game.maxPlayers <= maxPlayersRangeValues.end
-                      : true && maxPlayersRangeValues.start != 0
-                          ? game.maxPlayers >= maxPlayersRangeValues.start
+                          maxRangeValues.end != 0
+                      ? game.maxPlayers <= maxRangeValues.end
+                      : true && maxRangeValues.start != 0
+                          ? game.maxPlayers >= maxRangeValues.start
                           : true) {
                     if (onlyOwnedGames!) {
                       if (game.owned == 0) continue;
@@ -122,18 +116,7 @@ class _GameHelperState extends State<GameHelper> {
                 }
               },
             ),
-            Flexible(
-                flex: 1,
-                child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    child: Expanded(
-                        child: Text(
-                      chosenGame,
-                      textAlign: TextAlign.center,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(fontSize: 30),
-                    )))),
+            Text(chosenGame),
           ])
         ])));
   }
